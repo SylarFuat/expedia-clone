b01955ed4e41fad91b2c81e2f75efe8c62cc9a04
Fuat Altun <fuataltun08@gmail.com>
2024-11-06 12:17:19 -0800

    Delete Expedia/ProjectExpedia directory

--- Expedia/ProjectExpedia/rentalfind/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import VehicleCategory, Vehicle, Location
-
-# Register your models here.
-admin.site.register(VehicleCategory)
-admin.site.register(Vehicle)
-admin.site.register(Location)
--- Expedia/ProjectExpedia/userforms/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserformsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'userforms'
