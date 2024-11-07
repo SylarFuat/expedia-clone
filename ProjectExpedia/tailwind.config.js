54b0621e2527b37430560472afae5b19254ace5a
SylarFuat <fuataltun08@gmail.com>
2024-11-07 14:46:15 -0800

    Commit 6

---
+++ ProjectExpedia/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        backgroundColor: {
+          'custom-blue': '#1e3a8a', // Custom blue shade
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
